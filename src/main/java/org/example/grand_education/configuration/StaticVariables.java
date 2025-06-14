361e47b913ac7ec32f960e38d402a1221271d3ed
nazirov_istam <inazirov.unlimit@gmail.com>
2025-06-14 16:36:12 +0500

    Fixed bugs

--- src/main/java/org/example/grand_education/configuration/StaticVariables.java
+++ src/main/java/org/example/grand_education/configuration/StaticVariables.java
@@ -1,7 +1,7 @@
 package org.example.grand_education.configuration;
 
 public final class StaticVariables {
-    public final static String BOT_TOKEN = "8058060704:AAHmvU1VYjSumyn2kbRaZLMm6OQj9H234YY";
-    public final static String BOT_USERNAME = "grand_university_bot";
-    public final static Long OWNER_CHAT_ID = 1386819485L;
+    public final static String BOT_TOKEN = "BOT_TOKEN";
+    public final static String BOT_USERNAME = "BOT_USERNAME";
+    public final static Long OWNER_CHAT_ID = 1L;
 }
